ea9794b4bc5636298617dede006bb5ab74bb5ce9
mac <ghayoor10th@gmail.com>
2020-05-04 20:48:42 +0500

    GitGardian Warning Resolved

--- Clima/Controller/WeatherManager.swift
+++ Clima/Controller/WeatherManager.swift
@@ -18,7 +18,8 @@ protocol WeatherManagerDelegate {
 
 
 struct WeatherManager {
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=e72ca729af228beabd5d20e3b7749713&units=metric"
+    // get the open weather API.
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=YOUR_API_KEY&units=metric"
     var delegate: WeatherManagerDelegate?
     func fetchWeather(cityName: String){
         let urlString = "\(weatherURL)&q=\(cityName)"
